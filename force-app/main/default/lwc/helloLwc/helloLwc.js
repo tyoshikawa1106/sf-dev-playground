43ccfe9111357ae741d4ed2fce3da5738b2f7538
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-08-21 16:46:22 +0900

    add lwc/helloLwc
    Lightning Web コンポーネント「helloLwc」を追加。

---
+++ force-app/main/default/lwc/helloLwc/helloLwc.js
@@ -0,0 +1,28 @@
+import { LightningElement,track } from 'lwc';
+
+export default class HelloLwc extends LightningElement {
+    @track name = 'Lightning';
+    @track isChecked = false;
+    @track members = [
+        {
+            id: "1",
+            name: "Sato"
+        },
+        {
+            id: "2",
+            name: "Suzuki"
+        },
+        {
+            id: "3",
+            name: "Tanaka"
+        }
+    ];
+
+    onNameChanged() {
+        this.name = this.template.querySelector('[data-id="inputName"]').value;
+    }
+
+    onCheckboxChanged() {
+        this.isChecked = this.template.querySelector('[data-id="inputCheckbox"]').checked;
+    }
+}
