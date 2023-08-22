9496327df43d2b189e72ae72b7e2c83ba8fd72a0
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-08-22 13:08:19 +0900

    update helloLwc
    内容を更新。

--- force-app/main/default/lwc/helloLwc/helloLwc.js
+++ force-app/main/default/lwc/helloLwc/helloLwc.js
@@ -1,8 +1,20 @@
 import { LightningElement,track } from 'lwc';
 
 export default class HelloLwc extends LightningElement {
-    @track name = '';
+    @track name = 'Lightning';
     @track isChecked = false;
+    @track isCheckboxButtonChecked = false;
+    @track birthday = null;
+    @track createdDateTime = null;
+    @track email = 'sample@example.com';
+    @track number = null;
+    @track password = '';
+    @track range = 30;
+    @track phone = '0X0-1234-1234';
+    @track time = null;
+    @track isToggleChecked = false;
+    @track url = '';
+    @track richText = '';
     @track members = [
         {
             id: "1",
@@ -24,5 +36,58 @@ export default class HelloLwc extends LightningElement {
 
     onCheckboxChanged() {
         this.isChecked = this.template.querySelector('[data-id="inputCheckbox"]').checked;
+    }
+
+    onCheckboxButtonChanged() {
+        this.isCheckboxButtonChecked = this.template.querySelector('[data-id="inutCheckboxButton"]').checked;
+    }
+
+    onDateChanged() {
+        this.birthday = this.template.querySelector('[data-id="inputDate"]').value;
+    }
+
+    onDateTimeChanged() {
+        this.createdDateTime = this.template.querySelector('[data-id="inputDateTime"]').value;
+    }
+
+    onEmailChanged() {
+        this.email = this.template.querySelector('[data-id="inputEmail"]').value;
+    }
+
+    handleFilesChange() {
+        console.log('Files OK!');
+        console.log(this.template.querySelector('[data-id="inputFile"]'));
+    }
+
+    onNumberChanged() {
+        this.number = this.template.querySelector('[data-id="inputNumber"]').value;
+    }
+
+    onPasswordChanged() {
+        this.password = this.template.querySelector('[data-id="inputPassword"]').value;
+    }
+
+    onRangeChanged() {
+        this.range = this.template.querySelector('[data-id="inputRange"]').value;
+    }
+
+    onTelChanged() {
+        this.phone = this.template.querySelector('[data-id="inputTel"]').value;
+    }
+
+    onTimeChanged() {
+        this.time = this.template.querySelector('[data-id="inputTime"]').value;
+    }
+
+    onToggleChanged() {
+        this.isToggleChecked = this.template.querySelector('[data-id="inputToggle"]').checked;
+    }
+
+    onUrlChanged() {
+        this.url = this.template.querySelector('[data-id="inputUrl"]').value;
+    }
+
+    onRichTextChanged() {
+        this.richText = this.template.querySelector('[data-id="inputRichText"]').value;
     }
 }
