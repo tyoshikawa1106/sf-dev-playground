19fecad2b5c325d97e6c2057ecfa29c73e29a204
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-08-22 14:38:55 +0900

    add lwc/bike
    bikeコンポーネントを追加してappコンポーネントを更新。

--- force-app/main/default/lwc/app/app.js
+++ force-app/main/default/lwc/app/app.js
@@ -1,15 +1,8 @@
+// app.js
 import { LightningElement } from 'lwc';
 export default class App extends LightningElement {
-    name = 'Electra X4';
-    description = 'A sweet bike built for comfort.';
-    category = 'Mountain';
-    material = 'Steel';
-    price = '$2,700';
-    pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
-    ready = false;
-    connectedCallback() {
-        setTimeout(() => {
-            this.ready = true;
-        }, 3000);
-    }
+  bike = {
+    name: 'Electra X4',
+    picture: 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg'
+  };
 }
