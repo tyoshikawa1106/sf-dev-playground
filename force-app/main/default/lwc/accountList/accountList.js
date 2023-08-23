be223666ae6f69221802accefbb5133b0f219784
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-08-23 11:01:40 +0900

    update salesforce dx project
    プロジェクトをアップデート。

--- force-app/main/default/lwc/accountList/accountList.js
+++ force-app/main/default/lwc/accountList/accountList.js
@@ -3,6 +3,7 @@ import NAME_FIELD from '@salesforce/schema/Account.Name';
 import REVENUE_FIELD from '@salesforce/schema/Account.AnnualRevenue';
 import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
 import getAccounts from '@salesforce/apex/AccountController.getAccounts';
+import { reduceErrors } from 'c/ldsUtils';
 const COLUMNS = [
     { label: 'Account Name', fieldName: NAME_FIELD.fieldApiName, type: 'text' },
     { label: 'Annual Revenue', fieldName: REVENUE_FIELD.fieldApiName, type: 'currency' },
@@ -12,4 +13,9 @@ export default class AccountList extends LightningElement {
     columns = COLUMNS;
     @wire(getAccounts)
     accounts;
+
+    get errors() {
+        return (this.accounts.error) ?
+            reduceErrors(this.accounts.error) : [];
+    }
 }
