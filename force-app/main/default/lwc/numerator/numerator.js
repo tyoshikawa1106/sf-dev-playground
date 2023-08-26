40d82636af25f3f9eadc7ebcaafab2a707de287d
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-08-26 09:28:24 +0900

    commit

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,7 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
-    counter = 0;
+    //@api counter = 0;
+    _currentCount = 0;
+    priorCount = 0;
+    @api
+    get counter() {
+      return this._currentCount;
+    }
+    set counter(value) {
+      this.priorCount = this._currentCount;
+      this._currentCount = value;
+    }
     handleIncrement() {
       this.counter++;
     }
@@ -11,5 +21,9 @@ export default class Numerator extends LightningElement {
     handleMultiply(event) {
         const factor = event.detail;
         this.counter *= factor;
+    }
+    @api
+    maximizeCounter() {
+      this.counter += 1000000;
     }
 }
