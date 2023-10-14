60a0f82b170ab9a1b56aa44284eae9a8af5dcdbb
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-10-14 10:26:49 +0900

    commit

---
+++ force-app/main/default/lwc/libsSVG/libsSVG.js
@@ -0,0 +1,6 @@
+import { LightningElement } from 'lwc';
+import SVG_LOGO from "@salesforce/resourceUrl/svgLogo";
+
+export default class LibsSVG extends LightningElement {
+    svgURL = `${SVG_LOGO}#logo`;
+}
