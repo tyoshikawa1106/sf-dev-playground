443a677eee6d6f9de3ed32bc0b1bae8d9faeed2b
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2023-12-31 12:39:08 +0900

    commit
    LWCサンプルコードを追加。
    Salesforceプロジェクトをアップデート。

---
+++ force-app/main/default/lwc/lwcHelloWorld/lwcHelloWorld.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class LwcHelloWorld extends LightningElement {
+    greeting = 'World';
+    changeHandler(event) {
+        this.greeting = event.target.value;
+    }
+}
