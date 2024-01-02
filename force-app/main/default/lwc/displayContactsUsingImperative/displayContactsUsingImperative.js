00c6a41bc13f354963e6a93c6bde41cfed7fee2d
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2024-01-03 07:59:55 +0900

    commit

---
+++ force-app/main/default/lwc/displayContactsUsingImperative/displayContactsUsingImperative.js
@@ -0,0 +1,17 @@
+import { LightningElement } from 'lwc';
+import getContactList from "@salesforce/apex/DisplayContactsUsingImperativeController.getContactList";
+
+export default class DisplayContactsUsingImperative extends LightningElement {
+    contacts;
+    error;
+
+    handleLoad() {
+        getContactList()
+        .then((result) => {
+            this.contacts = result;
+        })
+        .catch((error) => {
+            this.error = error;
+        });
+    }
+}
