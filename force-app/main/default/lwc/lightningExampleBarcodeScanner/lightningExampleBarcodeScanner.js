ce41d21868c65d92250f210085436e28be3da171
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2024-01-06 15:24:25 +0900

    commit

---
+++ force-app/main/default/lwc/lightningExampleBarcodeScanner/lightningExampleBarcodeScanner.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+const myScannerOptions = {
+    barcodeTypes: ["CODE_128", "QR", "EAN_13"],
+    scannerSize: "LARGE",
+};
+
+export default class LightningExampleBarcodeScanner extends LightningElement {
+    
+}
