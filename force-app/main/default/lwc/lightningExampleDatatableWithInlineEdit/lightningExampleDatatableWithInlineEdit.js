2ab26c72a13e9466ec98ece44653a9696890e796
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2024-01-07 15:49:53 +0900

    commit

---
+++ force-app/main/default/lwc/lightningExampleDatatableWithInlineEdit/lightningExampleDatatableWithInlineEdit.js
@@ -0,0 +1,20 @@
+import { LightningElement } from 'lwc';
+import generateData from './generateData';
+
+const columns = [
+    { label: 'Label', fieldName: 'name', editable: true },
+    { label: 'Website', fieldName: 'website', type: 'url', editable: true },
+    { label: 'Phone', fieldName: 'phone', type: 'phone', editable: true },
+    { label: 'CloseAt', fieldName: 'closeAt', type: 'date', editable: true },
+    { label: 'Balance', fieldName: 'amount', type: 'currency', editable: true },
+];
+
+export default class LightningExampleDatatableWithInlineEdit extends LightningElement {
+    data = [];
+    columns = columns;
+    rowOffset = 0;
+
+    connectedCallback() {
+        this.data = generateData({ amountOfRecords: 100 });
+    }
+}
